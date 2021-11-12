cb8e3d86378c8d1ecd0f45bc01d9f6454461391a
kajfik <kajf.jakub@gmail.com>
2021-11-12 10:24:04 +0100

    Added decoding compatibility for new save format

--- js/save_convertor.js
+++ js/save_convertor.js
@@ -1,6 +1,8 @@
 var mobile_save_textarea = document.getElementById("mobile_save");
 var json_textarea = document.getElementById("json");
 var pako = require('pako');
+var startingString = "AntimatterDimensionsAndroidSaveFormat";
+var endingString = "EndOfSavefile";
 
 function onMobileSaveInput() {
     var mobile_save = mobile_save_textarea.value;
@@ -8,30 +10,34 @@ function onMobileSaveInput() {
     if (mobile_save === "") {
         json_textarea.value = "";
     } else {
-        var base64_string = mobile_save.replace(/\-/g, "+").replace(/\_/g, "/");
-        var uintArray = Base64Binary.decode(base64_string);  
-        var output = pako.ungzip(uintArray, { to: 'string' });
+        var base64_string = "";
+        if (mobile_save.startsWith(startingString)) {
+            const trimmed = mobile_save.substring(startingString.length + 3).slice(0, -endingString.length);
+            base64_string = trimmed.replace(/0c/g, "/").replace(/0b/g, "+").replace(/0a/g, "0")
+        } else {
+            base64_string = mobile_save.replace(/\-/g, "+").replace(/\_/g, "/");
+        }
+        const uintArray = Base64Binary.decode(base64_string);  
+        const output = pako.ungzip(uintArray, { to: 'string' });
         json_textarea.value = output;
     }
-    
 }
 
 function onJsonInput() {
-    var json = json_textarea.value;
+    const json = json_textarea.value;
 
     if (json === "") {
         mobile_save_textarea.value = "";
     } else {
-        var gzipped_json = pako.gzip(json);
-
+        const gzipped_json = pako.gzip(json);
         mobile_save_textarea.value = _arrayBufferToBase64(gzipped_json).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
     }
 }
 
 function _arrayBufferToBase64( buffer ) {
     var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
+    const bytes = new Uint8Array( buffer );
+    const len = bytes.byteLength;
     for (var i = 0; i < len; i++) {
         binary += String.fromCharCode( bytes[ i ] );
     }
